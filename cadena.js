7223caa94c373c5404486c4fb7a1276461415fb1
JESUS <fajardosullcajesus@gmail.com>
2020-05-16 20:46:43 -0400

    cadenalarga culminado

--- cadena.js
+++ cadena.js
@@ -1,6 +1,9 @@
 function cadenalarga(str){
-    let words = str.split("");
-    let maxSize = Max.max(...words.map(word =>word.length))
+    let words = str.split(" ");
+    let maxSize = Math.max(...words.map(word =>word.length))
     return words.filter(word => word.length === maxSize );
     return maxSize;
 }
+console.log(cadenalarga("aba , aa , ad , vcd , aba"))
+console.log(cadenalarga("enyky , benyky , yely , varennyky "))
+export {cadenalarga}
