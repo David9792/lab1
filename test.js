11183203681faa742e7ebe667ea48ea6f0134e64
JESUS <fajardosullcajesus@gmail.com>
2020-05-16 23:48:57 -0400

    invertida terminada

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ console.log(cadenalarga("aba , aa , ad , vcd , aba"))
 console.log(cadenalarga("enyky , benyky , yely , varennyky "))
 
 
-//import {invertir} from "./invertida.js";
-//console.log(invertir("(bar)"))
-//console.log(invertir("foo(bar)baz"))
-//console.log(invertir("foo(bar)baz(blim)"))
-//console.log(invertir("foo(bar(baz))blim"))
+import {invertir} from "./invertida.js";
+console.log(invertir("(bar)"))
+console.log(invertir("foo(bar)baz"))
+console.log(invertir("foo(bar)baz(blim)"))
+console.log(invertir("foo(bar(baz))blim"))
